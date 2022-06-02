10977d88a92b94eb45eeaae6c654472e263969f3
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-06-02 11:07:13 +0100

    Add project files.

---
+++ HTMLEditX/MainEditor.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace HTMLEditX
+{
+    partial class MainEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainEditor));
+            this.rtbEditor = new System.Windows.Forms.RichTextBox();
+            this.btnTemplate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbEditor
+            // 
+            this.rtbEditor.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbEditor.Location = new System.Drawing.Point(156, 0);
+            this.rtbEditor.Name = "rtbEditor";
+            this.rtbEditor.Size = new System.Drawing.Size(540, 570);
+            this.rtbEditor.TabIndex = 0;
+            this.rtbEditor.Text = "";
+            this.rtbEditor.TextChanged += new System.EventHandler(this.rtbEditor_TextChanged);
+            // 
+            // btnTemplate
+            // 
+            this.btnTemplate.Location = new System.Drawing.Point(729, 12);
+            this.btnTemplate.Name = "btnTemplate";
+            this.btnTemplate.Size = new System.Drawing.Size(76, 80);
+            this.btnTemplate.TabIndex = 1;
+            this.btnTemplate.Text = "Add HTML Template";
+            this.btnTemplate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTemplate.UseVisualStyleBackColor = true;
+            this.btnTemplate.Click += new System.EventHandler(this.btnTemplate_Click);
+            // 
+            // MainEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 568);
+            this.Controls.Add(this.btnTemplate);
+            this.Controls.Add(this.rtbEditor);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainEditor";
+            this.Text = "WebEditX";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbEditor;
+        private System.Windows.Forms.Button btnTemplate;
+    }
+}
+
---
+++ HTMLEditX/MainEditor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTMLEditX
+{
+    public partial class MainEditor : Form
+    {
+        public MainEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void rtbEditor_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnTemplate_Click(object sender, EventArgs e)
+        {
+            //Bodged approach to adding new lines, don't ask why
+
+            string newLine = Environment.NewLine;
+            rtbEditor.AppendText(" <!DOCTYPE html>" + newLine);
+            rtbEditor.AppendText("<html>" + newLine);
+            rtbEditor.AppendText("<head>" + newLine);
+            rtbEditor.AppendText("<title>Hello World!<title>" + newLine);
+            rtbEditor.AppendText("</head>" + newLine);
+            rtbEditor.AppendText("<body>" + newLine);
+            rtbEditor.AppendText(newLine);
+            rtbEditor.AppendText("<h1>Hello World/<h1>");
+        }
+    }
+}
