32b678dc26ec6d1c80411eec8b1714200cead951
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-06-04 12:33:40 +0100

    Removed uneccessary using directives

--- HTMLEditX/MainEditor.cs
+++ HTMLEditX/MainEditor.cs
@@ -14,13 +14,6 @@
     along with this program.  If not, see https://www.gnu.org/licenses/. */
 using System;
 using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HTMLEditX
