036b844c05732d22c48ac553a73f951a507621a0
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-06-08 18:25:35 +0100

    Removed useless file manager

--- HTMLEditX/MainEditor.Designer.cs
+++ HTMLEditX/MainEditor.Designer.cs
@@ -54,14 +54,12 @@ namespace HTMLEditX
             this.btnAddbtn = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.optionsPanel = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.btnFont = new System.Windows.Forms.Button();
             this.btnOpen = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnPreview = new System.Windows.Forms.Button();
             this.btnTemplate = new System.Windows.Forms.Button();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.optionsPanel.SuspendLayout();
@@ -147,7 +145,6 @@ namespace HTMLEditX
             // optionsPanel
             // 
             this.optionsPanel.BackColor = System.Drawing.Color.Gray;
-            this.optionsPanel.Controls.Add(this.button1);
             this.optionsPanel.Controls.Add(this.btnFont);
             this.optionsPanel.Controls.Add(this.btnOpen);
             this.optionsPanel.Controls.Add(this.label1);
@@ -156,16 +153,6 @@ namespace HTMLEditX
             this.optionsPanel.Name = "optionsPanel";
             this.optionsPanel.Size = new System.Drawing.Size(152, 144);
             this.optionsPanel.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(3, 103);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 41);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "File Manager";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnFont
             // 
@@ -260,8 +247,6 @@ namespace HTMLEditX
         private System.Windows.Forms.Panel optionsPanel;
         private System.Windows.Forms.Button btnFont;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnAddbtn;
         private System.Windows.Forms.Label label2;
