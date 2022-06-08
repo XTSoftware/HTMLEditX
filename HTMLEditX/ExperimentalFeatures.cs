b99b290fbdf14d5f555012331c6751352dd22987
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-06-08 18:34:40 +0100

    Added experimental feature - file manager

---
+++ HTMLEditX/ExperimentalFeatures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTMLEditX
+{
+    public partial class ExperimentalFeatures : Form
+    {
+        public ExperimentalFeatures()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileMan fm = new FileMan();
+            fm.ShowDialog();
+        }
+    }
+}
