b6deb6e1f6d0915a68880a1bca9a4a0fc629ebc7
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-06-10 18:59:54 +0100

    Begun UI rework (most features not readded yet)

--- HTMLEditX/MainEditor.cs
+++ HTMLEditX/MainEditor.cs
@@ -23,10 +23,36 @@ namespace HTMLEditX
         public MainEditor()
         {
             InitializeComponent();
-            
+
         }
 
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            DialogResult FontResult = fontDialog1.ShowDialog();
 
+            if (FontResult != DialogResult.Cancel)
+            {
+                rtbEditor.Font = fontDialog1.Font;
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Title = "Save";
+            saveDialog.Filter = "Hypertext Markup Language (HTML) Files (*.html)|*.html" + "|" +
+
+                                "Plain Text Files (*.txt)|*.txt" + "|" + "All files (*.*)|*.*";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filetosave = saveDialog.FileName;
+                System.IO.StreamWriter writer = new System.IO.StreamWriter(filetosave);
+                writer.Write(rtbEditor.Text.ToString());
+                writer.Close();
+                writer.Dispose();
+            }
+        }
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
@@ -45,105 +71,7 @@ namespace HTMLEditX
             }
         }
 
-        // _1 is because of UI change
-        private void btnTemplate_Click_1(object sender, EventArgs e)
-        {
-            //Bodged approach to adding new lines, don't ask why
-
-            string newLine = Environment.NewLine;
-            rtbEditor.AppendText(" <!DOCTYPE html>" + newLine);
-            rtbEditor.AppendText("<html>" + newLine);
-            rtbEditor.AppendText("<head>" + newLine);
-            rtbEditor.AppendText("<title>This is displayed as the title</title>" + newLine);
-            rtbEditor.AppendText("</head>" + newLine);
-            rtbEditor.AppendText("<body>" + newLine);
-            rtbEditor.AppendText(newLine);
-            rtbEditor.AppendText("<h1>Hello World</h1>");
-        }
-
-        private void btnPreview_Click(object sender, EventArgs e)
-        {
-            System.IO.StreamWriter writer = new System.IO.StreamWriter(@"C:/Users/Public/preview.html");
-            writer.Write(rtbEditor.Text.ToString());
-            writer.Close();
-            writer.Dispose();
-            PreviewPane previewPane = new PreviewPane();
-            previewPane.ShowDialog();
-        }
-
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveDialog = new SaveFileDialog();
-            saveDialog.Title = "Save";
-            saveDialog.Filter = "Hypertext Markup Language (HTML) Files (*.html)|*.html" + "|" +
-
-                                "Plain Text Files (*.txt)|*.txt" + "|" + "All files (*.*)|*.*" ;
-            if (saveDialog.ShowDialog() == DialogResult.OK)
-            {
-                string filetosave = saveDialog.FileName;
-                System.IO.StreamWriter writer = new System.IO.StreamWriter(filetosave);
-                writer.Write(rtbEditor.Text.ToString());
-                writer.Close();
-                writer.Dispose();
-            }
-        }
-        // _1 is because of UI change
-        private void btnOpen_Click_1(object sender, EventArgs e)
-        {
-            DialogResult result = openFileDialog1.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                string file = openFileDialog1.FileName;
-                try
-                {
-                    string text = File.ReadAllText(file);
-                    rtbEditor.Text = text;
-                }
-                catch (IOException)
-                {
-                }
-            }
-        }
-
-        private void btnFont_Click(object sender, EventArgs e)
-        {
-            DialogResult FontResult = fontDialog1.ShowDialog();
-
-            if (FontResult != DialogResult.Cancel)
-            {
-                rtbEditor.Font = fontDialog1.Font;
-            }
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnAddbtn_Click(object sender, EventArgs e)
-        {
-
-            // Makes new button in HTML with text "Button"
-            string newLine = Environment.NewLine;
-            rtbEditor.AppendText(newLine + " <button type=\"button\">button</button>");
-        }
-
-        private void btnJavaTemplate_Click(object sender, EventArgs e)
-        {
-            string newLine = Environment.NewLine;
-            rtbEditor.AppendText(newLine + "<script type = 'text/javascript'>");
-            rtbEditor.AppendText(newLine + "          // Write Javascript code here");
-            rtbEditor.AppendText(newLine + "</script>");
-        }
-
-        private void btnComment_Click(object sender, EventArgs e)
-        {
-            string newLine = Environment.NewLine;
-            rtbEditor.AppendText(newLine + "<!-- This is an example comment -->");
-        }
-
-        private void button1_Click_1(object sender, EventArgs e)
+        private void btnExperimental_Click(object sender, EventArgs e)
         {
             System.IO.StreamWriter writer = new System.IO.StreamWriter(@"C:/Users/Public/preview.html");
             writer.Write(rtbEditor.Text.ToString());
@@ -153,6 +81,9 @@ namespace HTMLEditX
             ef.ShowDialog();
         }
 
-    
+        private void darkSectionPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
