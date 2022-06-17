841dea19058d1e3aa12dd9b7455a9d8e01948ac1
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-06-17 18:41:34 +0100

    Removed Edge Preview from list of experimental features

--- HTMLEditX/ExperimentalFeatures.Designer.cs
+++ HTMLEditX/ExperimentalFeatures.Designer.cs
@@ -43,21 +43,10 @@ namespace HTMLEditX
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExperimentalFeatures));
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(13, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Edge preview";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -97,7 +86,6 @@ namespace HTMLEditX
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ExperimentalFeatures";
             this.Text = "Experimental Features";
@@ -108,7 +96,6 @@ namespace HTMLEditX
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
