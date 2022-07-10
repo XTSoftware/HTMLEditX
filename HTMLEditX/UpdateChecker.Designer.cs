f7bf91710cde46e4f11a2fa915e2a008a782709a
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-07-10 09:45:34 +0100

    Fixed MIT license messages

--- HTMLEditX/UpdateChecker.Designer.cs
+++ HTMLEditX/UpdateChecker.Designer.cs
@@ -1,11 +1,21 @@
 ﻿/* Copyright (c) 2022 XT Software
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions: */
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
 using Microsoft.Web.WebView2;
 namespace HTMLEditX
 {
@@ -39,7 +49,7 @@ namespace HTMLEditX
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateChecker));
             this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
-            
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
             // 
             // webView21
@@ -64,6 +74,8 @@ namespace HTMLEditX
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "UpdateChecker";
             this.Text = "Update Checker";
+            this.Load += new System.EventHandler(this.UpdateChecker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
 
         }
